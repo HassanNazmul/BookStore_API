83c8b4c6cdc56a8bf9de0a2ec007e562225bb64d
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2024-09-04 12:59:55 +0100

    Initial Commit

--- BookStoreApp/urls.py
+++ BookStoreApp/urls.py
@@ -1,12 +1,8 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import AuthorViewSet, CategoryViewSet, BookViewSet
+from django.urls import path
 
-router = DefaultRouter()
-router.register(r'authors', AuthorViewSet)
-router.register(r'categories', CategoryViewSet)
-router.register(r'books', BookViewSet)
+from BookStoreApp import views
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('books/', views.BookAPIView.as_view(), name='book_list'),
+    path('books/<int:id>/', views.BookAPIView.as_view(), name='book'),
 ]
