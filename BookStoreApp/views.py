9e96d3b4eec0178a10c4e70483c3a73181e5c9b2
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2024-09-08 23:45:54 +0100

    Initial Commit

--- BookStoreApp/views.py
+++ BookStoreApp/views.py
@@ -1,14 +1,1 @@
-import logging
-
-from django.db import transaction
-from django.http import Http404
-from django.shortcuts import get_object_or_404
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
-
-from BookStoreApp.models import Book
-from BookStoreApp.serializers import BookSerializer
-
-
 # Create your views here.
