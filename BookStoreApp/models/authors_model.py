fe5fa897171f12a57782c1dc314b45d12fdbc0ba
Mahadi Hassan <itsmahadi@gmail.com>
2024-09-08 17:01:06 +0600

    Update authors_model.py

--- BookStoreApp/models/authors_model.py
+++ BookStoreApp/models/authors_model.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import UserManager
 from django.db import models
 
 
@@ -9,8 +8,6 @@ class Author(models.Model):
     first_name = models.CharField(max_length=50, blank=False, null=False)
     last_name = models.CharField(max_length=50, blank=False, null=False)
     nationality = models.CharField(max_length=50, blank=True, null=True)
-
-    created_by = models.ForeignKey(UserManager, on_delete=models.CASCADE, related_name='author')
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
