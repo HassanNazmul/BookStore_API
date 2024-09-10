819e0f45a40811e77d23649d8ea89282e7831916
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2024-09-10 21:40:14 +0100

    Initial Commit

--- BookStoreApp/urls.py
+++ BookStoreApp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from BookStoreApp import views
 
 urlpatterns = [
-    path('books/', views.get_books)
+    path('books/', views.get_books),
+    path('books/<int:id>/', views.get_books)
 ]
--- BookStoreApp/views.py
+++ BookStoreApp/views.py
@@ -9,14 +9,24 @@ from BookStoreApp.serializers import BookSerializer
 # Create your views here.
 
 @api_view(['GET'])
-def get_books(request):
+def get_books(request, id=None):
     try:
-        # Get all books from the database
-        books = Book.objects.all().select_related('author')
-        # if no data is found in the database
-        if not books.exists():
-            return Response({'No books available in the database'}, status=status.HTTP_404_NOT_FOUND)
-        serializer = BookSerializer(books, many=True)
-        return Response(serializer.data)
+        if id:
+            # If an ID is provided, get the book with that ID
+            book = Book.objects.get(id=id)
+            serializer = BookSerializer(book)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            # If no ID is provided, get all books
+            books = Book.objects.all().select_related('author')
+            # If no books are found, return a message
+            if not books.exists():
+                return Response({'No Books Available in the Record'}, status=status.HTTP_200_OK)
+            serializer = BookSerializer(books, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+    # If the ID provided is invalid, return a message
+    except Book.DoesNotExist:
+        return Response({'Invalid ID in the URL'}, status=status.HTTP_404_NOT_FOUND)
+    # If any other error occurs, return the error message
     except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
